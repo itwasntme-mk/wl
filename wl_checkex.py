1195c77a6d621b174e5b8b1b93e1c7fdeb6ee8f1
itwasntme <dream.on@onet.pl>
2020-01-09 22:56:14 +0100

    Check statusVat entry

--- wl_checkex.py
+++ wl_checkex.py
@@ -13,8 +13,12 @@ def check_nip(subject, nip):
 
 def check_regon(subject, regon):
   return subject["regon"] == regon
-
-
+  
+  
+def check_vat_status(subject):
+  return subject["statusVat"] == "Czynny"
+  
+  
 def check(bank_account, nip=None, regon=None):
   if not nip and not regon or not bank_account:
     raise ValueError("Missing nip/regon or bank_account.")
@@ -33,28 +37,38 @@ def check(bank_account, nip=None, regon=None):
   result = response.json()["result"]
   
   #print(result)
-    
+
+  returnValue = [False, False, result["requestId"]]
+  
   if nip:
     nip = nip.replace("-", "")
     if not wl_tools.validate_nip(nip):
       raise ValueError("Invalid NIP")
-    if result["subjects"] is not None:
+    if "subjects" in result:
       for subject in result["subjects"]:
         if check_nip(subject, nip):
-          return (True, result["requestId"])
-    elif result["subject"] is not None:
-      return (check_nip(result["subject"], nip), result["requestId"])
+          returnValue[0] = True
+          returnValue[1] = "statusVat" in subject and check_vat_status(subject)
+          break
+    elif "subject" in result:
+      subject = result["subject"]
+      returnValue[0] = check_nip(subject, nip)
+      returnValue[1] = "statusVat" in subject and check_vat_status(subject)
   else:
     if not wl_tools.validate_regon(regon):
       raise ValueError("Invalid REGON")
-    if result["subjects"] is not None:
+    if "subjects" in result:
       for subject in result["subjects"]:
         if check_regon(subject, regon):
-          return (True, result["requestId"])
+          returnValue[0] = True
+          returnValue[1] = "statusVat" in subject and check_vat_status(subject)
+          break
     elif result["subject"] is not None:
-      return (check_regon(result["subject"], regon), result["requestId"])
+      subject = result["subject"]
+      returnValue[0] = check_regon(subject, regon)
+      returnValue[1] = "statusVat" in subject and check_vat_status(subject)
 
-  return (False, result["requestId"])
+  return returnValue
 
 
 def main():
@@ -71,7 +85,7 @@ def main():
   requestId = None
   
   try:
-    (result, requestId) = check(args.bank_account, args.nip, args.regon)
+    (result, status, requestId) = check(args.bank_account, args.nip, args.regon)
   except ValueError as ve:
     exit(ve)
 
@@ -79,6 +93,11 @@ def main():
     print("accountAssigned: TAK");
   else:
     print("accountAssigned: NIE");
+    
+  if status:
+    print("statusVat:       Czynny");
+  else:
+    print("statusVat:       Nieczynny");
 
   print("requestId:       " + requestId);
   
